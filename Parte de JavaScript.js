d74fb287ae6359f5bd5ba5e581408bca603bf9f5
K4ARP <cc26408@g.unicamp.br>
2026-08-10 21:14:29 -0300

    Parte de JavaScript.js

--- Parte de JavaScript.js
+++ Parte de JavaScript.js
@@ -13,4 +13,31 @@ for (let i = 0; i < 9; i++){
 
 container.appendChild(tab);
 
+const indicador_x = document.getElementById("indicando-x");
+const indicador_o = document.getElementById("indicando-o");
 
+let jogadorAtual = "x";
+const tabuleiro = ["","","","","","","","",""]
+
+tab.addEventListener("click", function(evento) {
+    const quadradoClicado = event.target;
+    if (!quadradoClicado.classList.contains("quadrado")) {
+        return;
+    }
+
+    const index = quadradoClicado.dataset.index;
+
+    if (tabuleiro[index] !== "") {
+        return;
+    }
+
+    tabuleiro[index] = jogadorAtual;
+    quadradoClicado.innerHTML = `<img src="${jogadorAtual}.svg" alt="${jogadorAtual}" class="marca">`;
+
+    jogadorAtual = jogadorAtual === "x" ? "o" : "x"
+
+    indicador_x.classList.toggle("ativo", jogadorAtual === "x");
+    indicador_o.classList.toggle("ativo", jogadorAtual === "o");
+
+
+});
